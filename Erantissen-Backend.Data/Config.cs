f5a2dd273970750706c08919d10a8756f43dcb72
TimurBas <timurbas1@hotmail.com>
2022-08-25 15:16:37 +0200

    removed config.cs

--- Erantissen-Backend.Data/Config.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Erantissen_Backend.Data
-{
-    internal static  class Config
-    {
-        public static readonly string DbConnectionString = @"Data Source=(localdb)\Erantissen;Initial Catalog=dbErantissen;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
-    }
-}
