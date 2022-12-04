e7e46053dd298888121780834c1cded8b2aec68e
TimurBas <timurbas1@hotmail.com>
2022-12-04 17:01:48 +0100

    Db config added

---
+++ Erantissen-Backend.Data/Config.cs
@@ -0,0 +1,7 @@
+﻿namespace Erantissen_Backend.Data
+{
+    internal class Config
+    {
+        public static string DbConnectionString = "Data Source=(LocalDb)\\Erantissen;Integrated Security=True";
+    }
+}
